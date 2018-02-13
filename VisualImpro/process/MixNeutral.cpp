3bad282af23186d8565ae66253c7075942dc1cf1
AlexCasanova <acasanova33560@hotmail.fr>
2018-02-13 17:29:18 +0100

    Un mix très commenté

--- VisualImpro/process/MixNeutral.cpp
+++ VisualImpro/process/MixNeutral.cpp
@@ -2,34 +2,34 @@
 
 #include "../utilities.hpp"
 
+using namespace std;
+
 extern "C"{
 
-void MixNeutral(void){
+/*
+vector <float> MixNeutral(int gSampleFactor, float coeff, BelaContext *context){
 
-	int gSampleFactor = 1;
-	gSampleFactor = gUserSet.sample_factor;
 	int STANDARD_SAMPLE_RATE = 22050;
-	// on a besoin de context
 
 	int contextAnalogFrames = context->analogFrames;
-	for(int i=0 ; i<contextAnalogFrames ; i++){
+	for(int n=0 ; n<contextAnalogFrames ; n++){
 		if (gSampleFactor == STANDARD_SAMPLE_RATE){       
-			audioWrite(context, 2*n, 0, out);
-    		        audioWrite(context, 2*n + 1, 0, out);
+			audioWrite(context, 2*n, 0, audioRead(context, 2*n, 0));
+    		        audioWrite(context, 2*n + 1, 0, audioRead(context, 2*n + 1, 0));
   		}
 		else{
-			audioWrite(context, n, 0, out);
+			audioWrite(context, n, 0, audioRead(context, n, 0));
     		}
 		
 		if (gSampleFactor == STANDARD_SAMPLE_RATE){
-			audioWrite(context, 2*n, 1, out);
-    		        audioWrite(context, 2*n + 1, 1, out);
+			audioWrite(context, 2*n, 1, audioRead(context, 2*n, 1));
+    		        audioWrite(context, 2*n + 1, 1, audioRead(context, 2*n + 1, 1));
   		}
 		else{
-			audioWrite(context, n, 1, out);
+			audioWrite(context, n, 1, audioRead(context, n, 1));
     		}
 	}
 
-}
+}*/
 
 }
