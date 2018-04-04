de954757205b50b8f599c560a1fc312a27f3b2eb
LucasVivas <lucas.vivas33@gmail.com>
2018-04-05 01:16:20 +0200

    delete RGB in GUI

--- GUI/installGUI.sh
+++ GUI/installGUI.sh
@@ -6,5 +6,9 @@ qmake-qt4 "QMAKE_CXXFLAGS += -std=c++11 -Wall" \
  "MOC_DIR = ./obj/" \
  "DESTDIR = ./bin" \
  "QT += network" \
+ "DEPENDPATH += ../VisualImpro/" \
+ "INCLUDEPATH += ../VisualImpro/" \
+ "SOURCES += RGB.cpp" \
+ "HEADERS += RGB.hpp"
   GUI.pro
 make
