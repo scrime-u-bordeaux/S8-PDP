8aba22455cb1c7c5e528be21aa6d1390fc6a9070
LucasVivas <lucas.vivas33@gmail.com>
2018-04-05 11:33:17 +0200

    avancement paul

--- GUI/src/mainWindow/GUIWindow.cpp
+++ GUI/src/mainWindow/GUIWindow.cpp
@@ -6,16 +6,14 @@
 
 #include "GUIWindow.hpp"
 
-GUIWindow::GUIWindow(int sizeMatrix, QWidget *parent) : QMainWindow(parent),
- view(new GUIImageMatrix(sizeMatrix, this)),
- server(new GUITCPServer()) {
+GUIWindow::GUIWindow(int sizeMatrix, QWidget *parent)
+    : QMainWindow(parent), view(new GUIImageMatrix(sizeMatrix, this)),
+      server(new GUITCPServer()) {
   setWindowTitle("Matrice");
   setCentralWidget(view);
-  resize((SQUARE_SIZE * sizeMatrix)+2, (SQUARE_SIZE * sizeMatrix)+2);
-  connect(server, SIGNAL(sendToGUI(vector<vector<RGB>>)), view, SLOT(updateColor(vector<vector<RGB>>)));
+  resize((SQUARE_SIZE * sizeMatrix) + 2, (SQUARE_SIZE * sizeMatrix) + 2);
+  connect(server, SIGNAL(sendToGUI(vector<vector<RGB>>)), view,
+          SLOT(updateColor(vector<vector<RGB>>)));
 }
 
-GUIWindow::~GUIWindow() {
-   delete view;
-
-}
+GUIWindow::~GUIWindow() { delete view; }
